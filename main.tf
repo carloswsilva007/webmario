58b91b5c6a522a0984ae08b87d138e8a68a63583
Carlos Wagner <carloswsilva007@gmail.com>
2022-01-03 09:18:18 -0300

    subindo projeto

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Baixar a imagem do Projeto Docker-SuperMario
+resource "docker_image" "image_id" {
+  name = "pengbai/docker-supermario:latest"
+}
+
+# Inicia o Container
+resource "docker_container" "container_id" {
+  name  = "supermario"
+  image = "${docker_image.image_id.latest}"
+  ports {
+    internal = "8080"
+    external = "80"
+  }
+}
+
